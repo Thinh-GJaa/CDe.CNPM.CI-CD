d541ed3e752e3b85a8bff879f23b11f258d0a2f1
Thinh-GJaa <xthinh04052002@gmail.com>
2024-06-21 14:33:33 +0700

    test fail

--- test/SampleTest.php
+++ test/SampleTest.php
@@ -5,10 +5,10 @@ use PHPUnit\Framework\TestCase;
 class SampleTest extends TestCase
 {
 
-    
+
     public function testSample()
     {
         
-        $this->assertTrue(true);
+        $this->assertT
     }
 }
